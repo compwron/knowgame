2d6928bb0b2902440d0ea5c233929da4f3e242d4
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-10-20 21:45:48 -0700

    cleaning up file output

--- lib/common_files.rb
+++ lib/common_files.rb
@@ -1,7 +1,9 @@
 class CommonFiles
 	attr_reader :files
 	def initialize(root, since_date, file_count)
-		@files = `git --git-dir=#{root}/.git --work-tree=#{root} log --pretty=format: --name-only --after #{since_date} | sort | uniq -c | sort -rg | head -#{file_count}`
+		command = "git --git-dir=#{root}/.git --work-tree=#{root} log --pretty=format: --name-only --after #{since_date} | sort | uniq -c | sort -rg | head -#{file_count}"
+		p command
+		@files = `#{command}`.split("\n").map {|line| line.strip!.split(" ")[1]}.compact
 		p files
 	end
 end
