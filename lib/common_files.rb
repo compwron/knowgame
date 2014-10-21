7bcb424353b843e9d3b0ed446b4633d97f79e2bb
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-10-20 21:42:20 -0700

    fixing git command to use optional root

--- lib/common_files.rb
+++ lib/common_files.rb
@@ -1,6 +1,7 @@
 class CommonFiles
-	def initialize(since_date, file_count)
-		files = `git log --pretty=format: --name-only --after #{since_date} | sort | uniq -c | sort -rg | head -#{file_count}`
+	attr_reader :files
+	def initialize(root, since_date, file_count)
+		@files = `git --git-dir=#{root}/.git --work-tree=#{root} log --pretty=format: --name-only --after #{since_date} | sort | uniq -c | sort -rg | head -#{file_count}`
 		p files
 	end
 end
