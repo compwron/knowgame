1d23b0267609a53cd29724696ff3d158cae54554
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-10-25 16:25:50 -0700

    gameplay

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,31 @@
 class Game
 	def initialize(common_files)
+		@common_files = common_files
+		# record keeper someday
 	end
 
 	def play
+		print_instructions
+		# pick a file
+		# find # of lines in file
+		# find non-empty line
+		# keep number
+		keep_going = true
+		while keep_going 
+			current = gets.chomp
+			puts "line is: JHLHL"
+			keep_going = (current != 'done')
+		end
+		print_end_report
+	end
+
+	def print_instructions
+		puts "Type 'done' to end game
+		To win the round, guess the filename which contains the line
+		To give up and get new data, type 'next'"
+	end
+
+	def print_end_report
+		"Game score: unknown"
 	end
 end
