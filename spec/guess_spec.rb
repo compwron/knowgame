be46e99fe8d2ded833e01512dfe16ce7167447ef
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-10-25 18:07:03 -0700

    renaming runfile

--- spec/guess_spec.rb
+++ spec/guess_spec.rb
@@ -4,10 +4,14 @@ describe Game do
   describe "#play" do
     it "should end game with 'done'" do
       g = Game.new(['file1.txt', 'file2.txt'], 5)
+
+      # allow(g).to receive(:gets).and_return('done')
+      # allow(g).to receive(:_random_line).and_return('a')
       g.stub(:gets).and_return('done')
       g.stub(:_random_line).and_return('a')
+
+      # expect { g.play }.to output(/.*Game score: {:correct=>0, :guesses=>0}/).to_stdout
       g.play
-      
     end
    end
 end
