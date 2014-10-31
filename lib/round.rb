7b99d53a20a73b448118fe29ce7fc55f0df93f6d
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-10-31 14:07:16 -0700

    Messy implementation of Round not hooked into game yet

--- lib/round.rb
+++ lib/round.rb
@@ -2,17 +2,39 @@ class Round
   def initialize(filename, remaining_tries)
     @filename = filename
     @remaining_tries = remaining_tries
+
+    @next_round = @game_over = @correct = false
+    @wrong_guesses = 0
   end
 
-  def guess filename
+  def guess guessed_filename
+  	if @filename == guessed_filename then
+  		@correct = @next_round = true
+  	else
+  		@wrong_guesses += 1
+  	end
+
+  	if @wrong_guesses == @remaining_tries then
+  		@next_round = true
+  	end
+
+  	@next_round = true if guessed_filename == 'next'
+
+  	if guessed_filename == 'done' then
+  	 	@game_over = true
+  	 	@next_round = false
+  	 end
   end
 
   def next_round?
+  	@next_round
   end
 
   def game_over?
+  	@game_over
   end
 
   def correct?
+  	@correct
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -16,32 +16,32 @@ describe Round do
     r.guess 'wrong guess'
     r.guess 'wrong guess'
 
-    expect(result.next_round?).to eq(true)
-    expect(result.game_over?).to eq(false)
-    expect(result.correct?).to eq(false)
+    expect(r.next_round?).to eq(true)
+    expect(r.game_over?).to eq(false)
+    expect(r.correct?).to eq(false)
   end
 
   it 'recognizes that one correct guess means continue to the next round' do
     r.guess 'foo.txt'
 
-    expect(result.next_round?).to eq(true)
-    expect(result.game_over?).to eq(false)
-    expect(result.correct?).to eq(true)
+    expect(r.next_round?).to eq(true)
+    expect(r.game_over?).to eq(false)
+    expect(r.correct?).to eq(true)
   end
 
   it 'recognizes command to move to next round' do
     r.guess 'next'
 
-    expect(result.next_round?).to eq(true)
-    expect(result.game_over?).to eq(false)
-    expect(result.correct?).to eq(false)
+    expect(r.next_round?).to eq(true)
+    expect(r.game_over?).to eq(false)
+    expect(r.correct?).to eq(false)
   end
 
   it 'recognizes command to move to end game' do
-    r.guess 'next'
+    r.guess 'done'
 
-    expect(result.next_round?).to eq(false)
-    expect(result.game_over?).to eq(true)
-    expect(result.correct?).to eq(false)
+    expect(r.next_round?).to eq(false)
+    expect(r.game_over?).to eq(true)
+    expect(r.correct?).to eq(false)
   end
 end
